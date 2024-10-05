cd25f32059c3d0bd2085a11e5954ae5021e07103
timmy <timmy0x@proton.me>
2024-10-05 19:55:05 -0400

    init

---
+++ Makefile
@@ -0,0 +1,5 @@
+
+
+flex_counter:
+	vlog -sv ./src/testbench/flex_counter_tb.sv ./src/modules/flex_counter.sv
+	vsim -voptargs="+acc" work.flex_counter_tb
---
+++ src/modules/flex_counter.sv
@@ -0,0 +1,15 @@
+
+module flex_counter #( parameter N=16 ) (
+    input logic nrst,
+    input logic clk,
+    input logic enable,
+    output logic[0:N-1] count
+);
+
+always_ff @( posedge clk, negedge nrst ) begin
+    if (~nrst) count <= '0;
+    else count <= count + enable;
+end
+
+
+endmodule
