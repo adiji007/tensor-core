4326b620744431ec868720af4a8a1d3df201d7bf
William Wong <wong371@asicfab.ecn.purdue.edu>
2025-02-16 16:41:41 -0500

    progress on memory arbiter

--- src/modules/memory_arbiter_basic.sv
+++ src/modules/memory_arbiter_basic.sv
@@ -2,100 +2,119 @@ module memory_arbiter_basic(
   input logic CLK, nRST
 );
 
-  // Variable Initialization
-  logic sreq, sWEN, sREN, swait,
-        ireq,       iREN, iwait,
-        dreq, dWEN, dREN, dwait;
-  logic next_sreq, next_sWEN, next_sREN, next_swait,
-        next_ireq,            next_iREN, next_iwait,
-        next_dreq, next_dWEN, next_dREN, next_dwait;
+  //TODO: TURN ALL OF THIS INTO AN INTERFACE
+  // Outputs to scratchpad
+  logic[63:0] load_data, next_load_data;
+  logic       sLoad_hit, sStore_hit, next_sLoad_hit, next_sStore_hit;
+  logic[1:0]  sLoad_row, next_sLoad_row;
   
+  // Inputs to arbiter from scratchpad
+  logic[63:0] store_data, next_store_data;
+  logic[31:0] load_addr, store_addr, next_load_addr, next_store_addr;
+  logic       sLoad, sStore, next_sLoad, next_sStore;
+
+  // Inputs to arbiter from scheduler
+  logic       dreq, ireq;
+  // Outputs to scheduler
+
+  //Main Memory Inputs & Outputs
+  logic       ramstore, ramaddr, ramWEN, ramREN;
+  logic       iwait, dwait, swait, iload, dload, sload;
+
   // State definitions
   typedef enum logic [2:0] {
       IDLE     = 3'b000,
-      GRANT_SP = 3'b001,
-      GRANT_I  = 3'b010,
-      GRANT_D  = 3'b011,
-      ACCESS   = 3'b100
+      SP_LOAD  = 3'b001,
+      SP_STORE = 3'b010,
+      DCACHE   = 3'b011,
+      ICACHE   = 3'b100
   } arbiter_states;
 
   arbiter_states arbiter_state, next_arbiter_state;
 
   always_ff @ (posedge CLK, negedge nRST) begin
     if(!nRST) begin
-      sreq = '0;
-      sWEN = '0;
-      sREN = '0;
-      swait = '0;
-      ireq = '0;
-      iREN = '0;
-      iwait = '0;
-      dreq = '0;
-      dWEN = '0;
-      dREN = '0;
-      dwait = '0;
-      arbiter_state = IDLE;
+      // Reset values for all signals
+      arbiter_state <= IDLE;
+      load_data <= 64'b0;
+      sLoad_hit <= 1'b0;
+      sStore_hit <= 1'b0;
+      sLoad_row <= 2'b00;
+      store_data <= 64'b0;
+      load_addr <= 32'b0;
+      store_addr <= 32'b0;
+      sLoad <= 1'b0;
+      sStore <= 1'b0;
     end
     else begin
-      sreq = next_sreq;
-      sWEN = next_sWEN;
-      sREN = next_sREN;
-      swait = next_swait;
-      ireq = next_ireq;
-      iREN = next_iREN;
-      iwait = next_iwait;
-      dreq = next_dreq;
-      dWEN = next_dWEN;
-      dREN = next_dREN;
-      dwait = next_dwait;
-      arbiter_state = next_arbiter_state;
+      // Update state and signals based on next_ values
+      arbiter_state <= next_arbiter_state;
+      load_data <= next_load_data;
+      sLoad_hit <= next_sLoad_hit;
+      sStore_hit <= next_sStore_hit;
+      sLoad_row <= next_sLoad_row;
+      store_data <= next_store_data;
+      load_addr <= next_load_addr;
+      store_addr <= next_store_addr;
+      sLoad <= next_sLoad;
+      sStore <= next_sStore;
     end
   end
+
+
 
   //NEXT STATE LOGIC
   always_comb begin
     case(arbiter_state)
       IDLE: begin
-        if(sreq) begin
-          next_arbiter_state = GRANT_SP;
+        if(sLoad) begin
+          next_arbiter_state = SP_LOAD;
         end
-        else if(dreq && !sreq) begin
-          next_arbiter_state = GRANT_D;
+        else if(sStore) begin
+          next_arbiter_state = SP_STORE;
         end
-        else if(ireq && !dreq && !sreq) begin
-          next_arbiter_state = GRANT_I;
+        else if(dreq) begin
+          next_arbiter_state = DCACHE;
+        end
+        else if(ireq) begin
+          next_arbiter_state = ICACHE;
         end
       end
 
-      GRANT_SP: begin
+      SP_LOAD: begin
         if(swait) begin
-          next_arbiter_state = GRANT_SP;
+          next_arbiter_state = SP_LOAD;
         end
         else begin
-          next_arbiter_state = ACCESS;
+          next_arbiter_state = IDLE;
         end
       end
 
-      GRANT_I: begin
-        if(iwait) begin
-          next_arbiter_state = GRANT_I;
+      SP_STORE: begin
+        if(swait) begin
+          next_arbiter_state = SP_STORE;
         end
         else begin
-          next_arbiter_state = ACCESS;
+          next_arbiter_state = IDLE;
         end
       end
 
-      GRANT_D: begin
+      DCACHE: begin
         if(dwait) begin
-          next_arbiter_state = GRANT_D;
+          next_arbiter_state = DCACHE;
         end
         else begin
-          next_arbiter_state = ACCESS;
+          next_arbiter_state = IDLE;
         end
       end
 
-      ACCESS: begin
-        next_arbiter_state = IDLE;
+      ICACHE: begin
+        if(iwait) begin
+          next_arbiter_state = ICACHE;
+        end
+        else begin
+          next_arbiter_state = IDLE;
+        end
       end
     endcase
   end
@@ -105,35 +124,46 @@ module memory_arbiter_basic(
   //TODO: COMMUNICATION WITH OTHER MODULES
   //OUTPUT LOGIC
   always_comb begin
-    next_sreq = sreq;
-    next_sWEN = sWEN;
-    next_sREN = sREN;
-    next_swait = swait;
-    next_ireq = ireq;
-    next_iREN = iREN;
-    next_iwait = iwait;
-    next_dreq = dreq;
-    next_dWEN = dWEN;
-    next_dREN = dREN;
-    next_dwait = dwait;
+    next_load_data = '0;
+    next_sLoad_hit = '0;
+    next_sStore_hit = '0;
+    next_sLoad_row = '0;
+    next_store_data = '0;
+    next_load_addr = '0;
+    next_store_addr = '0;
+    next_sLoad = '0;
+    next_sStore = '0;
     case(arbiter_state)
       IDLE: begin
+        if(sLoad) begin
+          next_load_addr = load_addr;
+        end
+        else if(sStore) begin
+          next_arbiter_state = SP_STORE;
+        end
+        else if(dreq) begin
+          next_arbiter_state = DCACHE;
+        end
+        else if(ireq) begin
+          next_arbiter_state = ICACHE;
+        end
+      end
+
+      SP_LOAD: begin
+        load_data = ramload; //TODO
+        next_sLoad_row = next_sLoad_row + 1;
+        
+      end
+
+      SP_STORE: begin
       
       end
 
-      GRANT_SP: begin
+      DCACHE: begin
       
       end
 
-      GRANT_I: begin
-      
-      end
-
-      GRANT_D: begin
-      
-      end
-
-      ACCESS: begin
+      ICACHE: begin
       
       end
     endcase
