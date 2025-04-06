0d9834392734800b2368eabeea53303121664995
Vinay Pundith <vpundith@asicfab.ecn.purdue.edu>
2025-04-06 12:39:42 -0400

    critical path optimizations

--- src/modules/ADD_step1.sv
+++ src/modules/ADD_step1.sv
@@ -14,7 +14,7 @@ module ADD_step1 (
     output logic rounding_loss
 );
 
-    reg  [4:0]   unsigned_exp_diff;
+    // logic  [4:0]   unsigned_exp_diff;
     reg    cmp_out;
     assign rounding_loss = 0;       // I originally had this to help with floating point rounding. Don't seem to need it though and it was increasing the critical path.
 
@@ -26,19 +26,24 @@ module ADD_step1 (
     // Comparing exponents (Logic copied over from what used to be int_compare.sv)
     wire [5:0] u_exp1 = {1'b0, floating_point1_in[14:10]};
     wire [5:0] u_exp2 = {1'b0, floating_point2_in[14:10]};
-    reg  [5:0] diff;
+    /* verilator lint_off UNUSEDSIGNAL */
+    reg  [5:0] diff, diff_reverse;
+    /* verilator lint_on UNUSEDSIGNAL */
 
-    assign unsigned_exp_diff = diff[4:0];
+    // assign unsigned_exp_diff = diff[4:0];
 
     always_comb begin
         diff = u_exp1 - u_exp2;
-        case (diff[5])
-            1'b0: cmp_out = 1'b0;
-            1'b1: begin
-                cmp_out = 1'b1;
-                diff = -diff;
-            end
-        endcase
+        diff_reverse = u_exp2 - u_exp1;
+        cmp_out = diff[5];
+
+        // case (diff[5])
+        //     1'b0: cmp_out = 1'b0;
+        //     1'b1: begin
+        //         cmp_out = 1'b1;
+        //         diff = -diff;
+        //     end
+        // endcase
     end
     //--------------------------------------------------------------------------------------------
     // Handling "implicit" 1/0 leading bit for fraction
@@ -62,7 +67,7 @@ module ADD_step1 (
 
     always_comb begin
         if(cmp_out == 1) begin
-            frac_shifted = {frac_leading_bit_fp1, floating_point1_in[9:0], 2'b00} >> unsigned_exp_diff;
+            frac_shifted = {frac_leading_bit_fp1, floating_point1_in[9:0], 2'b00} >> diff_reverse[4:0];
             // rounding_loss = |({frac_leading_bit_fp1, floating_point1_in[9:0], 2'b00} & ((1 << unsigned_exp_diff) - 1));    // chatgpt gave me this
             sign_shifted = floating_point1_in[15];
             frac_not_shifted = {frac_leading_bit_fp2, floating_point2_in[9:0], 2'b00};
@@ -70,7 +75,7 @@ module ADD_step1 (
             exp_max = floating_point2_in[14:10];
         end
         else begin
-            frac_shifted = {frac_leading_bit_fp2, floating_point2_in[9:0], 2'b00} >> unsigned_exp_diff;
+            frac_shifted = {frac_leading_bit_fp2, floating_point2_in[9:0], 2'b00} >> diff[4:0];
             // rounding_loss = |({frac_leading_bit_fp2, floating_point2_in[9:0], 2'b00} & ((1 << unsigned_exp_diff) - 1));
             sign_shifted = floating_point2_in[15];
             frac_not_shifted = {frac_leading_bit_fp1, floating_point1_in[9:0], 2'b00};
